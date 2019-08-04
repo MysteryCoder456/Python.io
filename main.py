8300172ff259a2abfe895f56b8ba86323bdc3b4e
Rehatbir Singh <rehatbir.singh@gmail.com>
2019-08-05 02:40:50 +0400

    Update menu screen

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
+from kivy.uix.widget import Widget
 
 
 #========================================================================================================#
@@ -23,13 +24,8 @@ from kivy.uix.button import Button
 #========================================================================================================#
 
 
-class MyGrid(GridLayout):
-	def __init__(self, **kwargs):
-		super(MyGrid, self).__init__(**kwargs)
-
-		self.cols = 1
-
-		self.add_widget(Label(text="hello"))
+class MyGrid(Widget):
+	pass
 
 
 class Menu(App):
@@ -283,6 +279,5 @@ def main():
 		
 if __name__ == "__main__":
 	main_menu()
-	main()
 	pygame.quit()
 	quit()
